750a55d5a609119c5acbe4861545bd26faae9b8c
Ferhat Nutku <ferhatcpp@gmail.com>
2022-07-20 10:29:00 +0300

    test_mechanics

--- tests/test_mechanics.py
+++ tests/test_mechanics.py
@@ -37,7 +37,7 @@ class sets:
     
     flow = [{100:"get_formulary", 150:"get_subformulary",
              200:"simple_harmonic_oscillator", 
-             2321:"coordinate_systems", 2322:"moving_particle"}[i] 
+             2321:"coordinate_systems"}[i] 
             for i in [200]]
 
 ### Formulary
@@ -68,34 +68,86 @@ if "simple_harmonic_oscillator" in sets.flow:
     omech.class_type = "scalar"
     omech.__init__()
     omech.solver.verbose = True
-    commands = ["solve", "NewtonsLaw2", a]
+    commands = ["solve", "NewtonsLaw2", omech.a.rhs]
     omech.process(commands)
 
 
     """
-    Example: Solve position of a spring.
+    Example: Solve position of a spring mass system.
     F = ma, F = -kx
     -kx = ma
     -kx = m d^2 x/dt^2
     w = sqrt(k/m)
     x(t) = C1*sin(wt) + C2*sin(wt)
     """
+    # Scalar Way.
+    omech.class_type = "scalar"
     omech.__init__()
     omech.solver.verbose = True
     display("Newton's 2nd Law", omech.NewtonsLaw2, 
             "Hooke's Law", omech.HookesLaw)
     commands = ["Eq", "NewtonsLaw2", "HookesLaw"]
     omech.process(commands)
-    commands = ["subs", "omech.result", [(a, diff(x, t, 2, evaluate=False))]]
+#    commands = ["subs", "omech.result", [(a, diff(x, t, 2, evaluate=False))]]
     res = omech.process(commands)
     simp = simplify(res.lhs/m)
     omech.result = Eq(simp, 0)
     commands = ["subs", "omech.result", [(k/m, w**2)]]
     omech.process(commands)
     commands = ["dsolve", "omech.result", x]
-    omech.process(commands)
-    
     print("Codes:\n", *omech.solver.get_codes())
+    
+    omech.x = omech.process(commands).rhs
+    v = omech.v.evalf(subs={x:omech.x}).doit()
+    a = omech.a.evalf(subs={x:omech.x}).doit()
+    display(omech.result,v,a)
+    
+    # Numerical calculations
+    [C1,C2] = symbols('C1 C2')
+    numvals = {C1:1, C2:1, w:2}
+#    commands = ["xreplace", "omech.x", numvals]
+#    omech.process(commands)
+    x = omech.x.evalf(subs=numvals).doit()
+    v = v.evalf(subs=numvals).rhs
+    a = a.evalf(subs=numvals).rhs
+    plot(x, (t,0,4*pi,200), xlabel="$t$", ylabel="$x(t)$")
+    plot(v, (t,0,4*pi,200), xlabel="$t$", ylabel="$v(t)$")
+    plot(a, (t,0,4*pi,200), xlabel="$t$", ylabel="$a(t)$")
+    plot_sympfunc([x.subs({t:var('x')}),], (0, float(4*pi), 200), xlabel="$t$", ylabel="$x(t)$")
+    
+    
+    # Vectorial Way. kaldik
+    omech.class_type = "vectorial"
+    omech.__init__()
+    omech.solver.verbose = True
+    commands = ["Eq", "NewtonsLaw2", "HookesLaw"]
+    omech.process(commands)
+#    commands = ["subs", "omech.result", [(a, diff(x, t, 2, evaluate=False))]]
+    res = omech.process(commands)
+    simp = simplify(res.lhs/m)
+    omech.result = Eq(simp, 0)
+    commands = ["subs", "omech.result", [(k/m, w**2)]]
+    omech.process(commands)
+    commands = ["dsolve", "omech.result", x]
+    print("Codes:\n", *omech.solver.get_codes())
+    
+    omech.x = omech.process(commands).rhs
+    v = omech.v.evalf(subs={x:omech.x}).doit()
+    a = omech.a.evalf(subs={x:omech.x}).doit()
+    display(omech.result,v,a)
+    
+    # Numerical calculations
+    [C1,C2] = symbols('C1 C2')
+    numvals = {C1:1, C2:1, w:2}
+#    commands = ["xreplace", "omech.x", numvals]
+#    omech.process(commands)
+    x = omech.x.evalf(subs=numvals).doit()
+    v = v.evalf(subs=numvals).rhs
+    a = a.evalf(subs=numvals).rhs
+    plot(x, (t,0,4*pi,200), xlabel="$t$", ylabel="$x(t)$")
+    plot(v, (t,0,4*pi,200), xlabel="$t$", ylabel="$v(t)$")
+    plot(a, (t,0,4*pi,200), xlabel="$t$", ylabel="$a(t)$")
+    plot_sympfunc([x.subs({t:var('x')}),], (0, float(4*pi), 200), xlabel="$t$", ylabel="$x(t)$")
     
 if "coordinate_systems" in sets.flow:
     print("Coordinate Systems")
@@ -105,24 +157,21 @@ if "coordinate_systems" in sets.flow:
     omech.__init__()
     omech.solver.verbose = False
     
-
+    xreplaces = {x:r*cos(theta)*C.i,
+                  y:r*sin(theta)*C.j,
+                  z:0}
     xreplaces = {x:omech.subformulary.pol_to_cart_x,
                  y:omech.subformulary.pol_to_cart_y,
                  z:0} # C.k
-    
-    # xreplaces = {x:r_t*cos(theta)*C.i,
-    #              y:r_t*sin(theta)*C.j,
-    #              z:0}
     display(omech.r, omech.v, omech.a)
     display(xreplaces)
 
     commands = ["xreplace", "omech.r", xreplaces]
-    x = omech.x.evalf(subs=xreplaces).doit()
-    y = omech.y.evalf(subs=xreplaces).doit()
-    z = omech.z.evalf(subs=xreplaces).doit()
-    r = omech.process(commands)
-    v = omech.v.evalf(subs=xreplaces).doit()
-    a = omech.a.evalf(subs=xreplaces).doit()
+    r = omech.process(commands).doit()
+    commands = ["xreplace", "omech.v", xreplaces]
+    v = omech.process(commands).doit()
+    commands = ["xreplace", "omech.a", xreplaces]
+    a = omech.process(commands).doit()
     display(x,y,z,r,v,a)
     
     print("Components of r")
@@ -131,9 +180,3 @@ if "coordinate_systems" in sets.flow:
     [display(v.rhs.args[i]) for i in range(2)]
     print("Components of a")
     [display(a.rhs.args[i]) for i in range(2)]
-            
-"""
-def main():    
-if __name__ == '__main__':
-    main()
-"""
